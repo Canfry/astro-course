2177f248138eafd8df8708ff8ced147edff71fcc
Christophe anfry <frybcn@gmail.com>
2023-12-14 22:12:11 +0100

    add all

---
+++ src/components/EmojiReactions.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { Emoji, emojiList } from '../types/Emoji';
+import EmojiReaction from './EmojiReaction.svelte';
+
+</script>
+
+<div class="flex gap-x-2 justify-center items-center h-8">
+
+    {#each emojiList as emoji}
+        <EmojiReaction name={emoji} reacted count={1} />
+    {/each}
+</div>
